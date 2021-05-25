8abd121e4c9c3e14bded9bf9b49d5526fa8b9524
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-05-25 22:41:00 +0530

    done

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import os
 import discord
 from pathlib import Path
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 load_dotenv(verbose=True)
 env_path = Path("./") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-client = discord.Client(command_prefix="!")
+client = commands.Bot(command_prefix="!")
+
 
 @client.event
 async def on_ready():
     print("Bot logged in!")
+
+
+@client.command(name="ping")
+async def ping(ctx):
+    await ctx.send("Pong")
 
 client.run(os.getenv("TOKEN"))
