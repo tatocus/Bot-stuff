dc1b46f8d061e4e219f10c447665d97795d1ea49
AakashSharma7269 <aakashsensharma04@gmail.com>
2021-05-25 22:18:03 +0530

    log in command ig or somethign idk

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import os
+import discord
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+load_dotenv(verbose=True)
+env_path = Path("./") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+client = discord.Client(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print("Bot logged in!")
+
+client.run(os.getenv("TOKEN"))
